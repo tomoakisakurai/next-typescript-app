47bd23bbf07ffba17e0ea4966b9dc48718e4f3ea
Tomoaki Sakurai <ssaku.power@gmail.com>
2020-04-20 16:37:00 +0900

    Delete unneccery files

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,28 @@
 module.exports = ({ config }) => {
-    config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        loader: require.resolve('babel-loader'),
-        options: {
-            presets: [require.resolve('babel-preset-react-app')], // 必要なパッケージがまとめて入る
-        },
-    });
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [require.resolve('babel-preset-react-app')], // 必要なパッケージがまとめて入る
+    },
+  })
 
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-};
+  // storybook で scss を load する
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1,
+          modules: true, // css modules での読み込みを許可する
+        },
+      },
+      'sass-loader',
+    ],
+  })
+
+  config.resolve.extensions.push('.ts', '.tsx')
+  return config
+}
